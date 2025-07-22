f2b97a510b084b471a0c3f48aca9909b287b8bf2
official16Dec <officialsunil16@gmail.com>
2025-07-22 09:06:11 +0530

    main.dart upgraded with google authentication implementation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:provider/provider.dart';
+import 'package:chat_via/services/firebase_auth_methods.dart';
+import 'package:chat_via/screens/signup_email_password_screen.dart';
+import 'package:chat_via/screens/login_email_password_screen.dart';
+import 'package:chat_via/screens/login_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -11,14 +22,44 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chat Via',
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
-      ),
-      home: HomeScreen(),
+    return MultiProvider(
+        providers: [
+          Provider<FirebaseAuthMethods>(
+            create: (_) => FirebaseAuthMethods(FirebaseAuth.instance),
+          ),
+          StreamProvider(
+            create: (context) => context.read<FirebaseAuthMethods>().authState,
+            initialData: null,
+          ),
+        ],
+        child:MaterialApp(
+          title: 'Chat Via',
+          theme: ThemeData.dark().copyWith(
+          primaryColor: Color(0xFF0A0E21),
+          scaffoldBackgroundColor: Color(0xFF0A0E21),
+        ),
+        home: const AuthWrapper(),
+        routes: {
+          EmailPasswordSignup.routeName: (context) =>
+          const EmailPasswordSignup(),
+          EmailPasswordLogin.routeName: (context) => const EmailPasswordLogin()
+        },
+      )
     );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User?>();
+
+    if (firebaseUser != null) {
+      return HomeScreen();
+    }
+    return const LoginScreen();
   }
 }
 
